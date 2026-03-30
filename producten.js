ec8e4344a9c8c19605246e38a7b173e9006d2d0b
Seppe Wouters <seppe.wouters@sintmaartencampus.be>
2026-03-30 10:05:44 +0200

    basis gedaan

--- producten.js
+++ producten.js
@@ -2,6 +2,7 @@ export let producten = [
     {
         id: 1,
         naam: "AMERICAN CLASSIC - Poloshirt",
+        afbeelding: "./html/ProductenImg/poloshirtWit.png",
         prijs: 3
     }
 ]
