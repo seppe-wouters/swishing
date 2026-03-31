a85bbca0eed7f7cd23f9a8de12c6c72370673d8f
Seppe Wouters <seppe.wouters@sintmaartencampus.be>
2026-03-31 14:23:14 +0200

    producten + winkel kar

---
+++ js/producten.js
@@ -0,0 +1,20 @@
+export let producten = [
+    {
+        id: 1,
+        naam: "AMERICAN CLASSIC - Poloshirt",
+        afbeelding: "../ProductenImg/poloshirtWit.png",
+        prijs: 3
+    }, 
+    {
+        id: 2,
+        naam: "Calvin Klein Underwear",
+        afbeelding: "../ProductenImg/CKUnderwear.png",
+        prijs: 5
+    },
+    {
+        id: 3,
+        naam: "AMERICAN CLASSIC TRACKWAY - Poloshirt",
+        afbeelding: "../ProductenImg/poloshirtZwart.png",
+        prijs: 3    
+    }
+]
